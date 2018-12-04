c4f134b10fbec142b4e9d673c02d52f6ecb5806e
Tony Martinez <ajosephmartinez@gmail.com>
2018-12-03 20:25:33 -0800

    .

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,11 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    return str.split(" ").map(word => {
+        return word.charAt(0).toUpperCase() 
+            + word.slice(1).join(" ");
+    });
+}
 
 module.exports = capitalize;
--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,26 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n, row = 0, level = '') {
+    if (row === n) {
+        return;
+    }
+
+    if (level.length === 2 * n - 1){
+        console.log(level);
+        return pyramid(n, row + 1);
+    }
+    const midpoint = Math.floor((2 * n - 1) / 2);
+    let add;
+    if (midpoint - row <= level.length && midpoint + row >= level.length) {
+        add = '#';
+    } else {
+        add = ' ';
+    }
+    debugger;
+    pyramid(n, row, level + add);
+}
+
+pyramid(5);
 
 module.exports = pyramid;
