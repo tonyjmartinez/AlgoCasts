3b886b7c68f08a3ffcb720edae7c332ce99d2dc8
Tony Martinez <ajosephmartinez@gmail.com>
2018-12-03 21:38:02 -0800

    .

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,19 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+//function vowels(str) {
+//    let count = 0;
+//    for (let chr of str.toLowerCase()) {
+//        if (chr.match(/[aeiou]/)) {
+//            count++;
+//        }
+//    }
+//    return count;
+//}
+
+function vowels(str) {
+    const matches = str.match(/[aeuiou]/gi);
+    return matches ? matches.length: 0;
+}
 
 module.exports = vowels;
