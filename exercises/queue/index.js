3c66cf495327ee778ed7db956498c3e0d2fc1a59
Tony Martinez <ajosephmartinez@gmail.com>
2018-12-11 07:51:22 -0800

    .

--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -8,6 +8,18 @@
 //     q.add(1);
 //     q.remove(); // returns 1;
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.q = [];
+  }
+
+  add(x) {
+    this.q.unshift(x);
+  }
+
+  remove() {
+    return this.q.pop();
+  }
+}
 
 module.exports = Queue;
